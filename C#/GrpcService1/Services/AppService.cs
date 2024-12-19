c177ff61097579f2bf4bfc318e9a9ef93b89f52e
ToniRakkaen <minhkhangg32@gmail.com>
2024-12-19 11:50:50 +0700

    add C# gRPC

---
+++ C#/GrpcService1/Services/AppService.cs
@@ -0,0 +1,23 @@
+﻿using Gosu.MobileGateway.Services;
+using Grpc.Core;
+
+namespace GrpcService1.Services
+{
+    public class AppService : grpcMobileGatewayService.grpcMobileGatewayServiceBase
+    {
+        private readonly ILogger<AppService> _logger;
+        public AppService(ILogger<AppService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<GetProfile_Response> GetProfile(GetProfile_Request request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetProfile_Response()
+            {
+                UserName = request.UserName,
+                Fullname = "Test fullname",
+            });
+        }
+    }
+}
---
+++ Java/gRPC_Demo/src/test/java/com/example/gRPC_Demo/GRpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gRPC_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
