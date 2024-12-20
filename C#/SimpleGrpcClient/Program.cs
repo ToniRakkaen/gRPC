17fc102bf4294b34fe22a4db9670e8867c27ecf4
ToniRakkaen <minhkhangg32@gmail.com>
2024-12-20 11:54:35 +0700

    create new react app + grpc C#

---
+++ C#/SimpleGrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+
+namespace SimpleGrpcClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grpcChannel = GrpcChannel.ForAddress("https://localhost:7263");
+            var greeterClient = new Greeter.GreeterClient(grpcChannel);
+            var greeterReply = greeterClient.SayHello(new HelloRequest
+            {
+                Name = "Khang",
+            });
+            Console.WriteLine(greeterReply.ToString);
+            Console.ReadKey();
+        }
+    }
+}
